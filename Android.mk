be8b553e0eed29a46c1ec7b937696216aebda51c
Etienne Carriere <etienne.carriere@linaro.org>
2020-01-27 13:01:47 +0100

    libckteec: rename libsks into libckteec
    Rename PKCS#11 client library from libsks into libckteec. Build
    scripts and source files are updated.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 ################################################################################
-# Android optee-client, libsks and tee-supplicant makefile                     #
+# Android optee-client, libckteec and tee-supplicant makefile                  #
 ################################################################################
 LOCAL_PATH := $(call my-dir)
 
@@ -32,14 +32,14 @@ LOCAL_CFLAGS += -DDEBUGLEVEL_$(CFG_TEE_CLIENT_LOG_LEVEL)
 LOCAL_CFLAGS += -DBINARY_PREFIX=\"TEEC\"
 
 LOCAL_SRC_FILES := libteec/src/tee_client_api.c \
-                   libteec/src/teec_trace.c
+		   libteec/src/teec_trace.c
 ifeq ($(CFG_TEE_BENCHMARK),y)
 LOCAL_CFLAGS += -DCFG_TEE_BENCHMARK
 LOCAL_SRC_FILES += teec_benchmark.c
 endif
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/public \
-                    $(LOCAL_PATH)/libteec/include
+		    $(LOCAL_PATH)/libteec/include
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE := libteec
@@ -51,31 +51,34 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/public
 
 include $(BUILD_SHARED_LIBRARY)
 
-# Build libsks.so
+################################################################################
+# Build libckteec.so - Shared library for CryptoKi API to a PKCS#11 OP-TEE token
+################################################################################
 include $(CLEAR_VARS)
-
 LOCAL_CFLAGS += $(optee_CFLAGS)
 
-LOCAL_SRC_FILES := libsks/src/pkcs11_api.c \
-                   libsks/src/ck_debug.c \
-                   libsks/src/ck_helpers.c \
-                   libsks/src/invoke_ta.c \
-                   libsks/src/pkcs11_token.c \
-                   libsks/src/serializer.c \
-                   libsks/src/serialize_ck.c \
-                   libsks/src/pkcs11_processing.c
+LOCAL_SRC_FILES := libckteec/src/pkcs11_api.c \
+		   libckteec/src/ck_debug.c \
+		   libckteec/src/ck_helpers.c \
+		   libckteec/src/invoke_ta.c \
+		   libckteec/src/pkcs11_token.c \
+		   libckteec/src/serializer.c \
+		   libckteec/src/serialize_ck.c \
+		   libckteec/src/pkcs11_processing.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/public \
-                    $(LOCAL_PATH)/libsks/include
+		    $(LOCAL_PATH)/libckteec/include
 
 LOCAL_SHARED_LIBRARIES := libteec
 LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE := libsks
+LOCAL_MODULE := libckteec
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/libsks/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/libckteec/include
 
 include $(BUILD_SHARED_LIBRARY)
 
-# TEE Supplicant
+################################################################################
+# TEE Supplicant - User space supplicant for TEE RPC service
+################################################################################
 include $(LOCAL_PATH)/tee-supplicant/tee_supplicant_android.mk
