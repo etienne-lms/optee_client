d957ac6b3e62d67e3ef652044154defdf5863c73
Etienne Carriere <etienne.carriere@linaro.org>
2020-03-16 16:23:40 +0100

    libcktee: ASSERT_CK_RV() on return value
    Define and use macro ASSERT_CK_RV() that is more convenient than
    explicit switch/case to assert API function return value compliance.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- libckteec/src/ck_helpers.c
+++ libckteec/src/ck_helpers.c
@@ -3,12 +3,32 @@
  * Copyright (c) 2017-2020, Linaro Limited
  */
 
+#include <assert.h>
+#include <ck_debug.h>
 #include <pkcs11_ta.h>
 #include <stdio.h>
 #include <string.h>
+#include <tee_client_api.h>
 
 #include "ck_helpers.h"
 #include "local_utils.h"
+
+#ifdef DEBUG
+void ckteec_assert_expected_rv(const char *function, CK_RV rv,
+			       const CK_RV *expected_rv, size_t expected_count)
+{
+	size_t n = 0;
+
+	for (n = 0; n < expected_count; n++)
+		if (rv == expected_rv[n])
+			return;
+
+	fprintf(stderr, "libckteec: %s: unexpected return value 0x%lx (%s)\n",
+		function, rv, ckr2str(rv));
+
+	assert(0);
+}
+#endif
 
 /*
  * PKCS11 TA returns Cryptoki like information structure.
